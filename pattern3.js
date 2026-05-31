3801cd919e786af6b96685c6a1c9f158381c2159
vikash sharma <119288896+vikashage21@users.noreply.github.com>
2026-05-31 17:41:56 +0000

    third pattern done

---
+++ pattern3.js
@@ -0,0 +1,17 @@
+let n = 4
+
+for (let i = 1; i <= n; i++) {
+    // row
+    let pattern = ''
+
+    for (let j = 1; j <= i; j++) {
+        pattern += j
+    }
+    console.log(pattern)
+}
+
+
+// 1
+// 12
+// 123
+// 1234

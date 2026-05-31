09c26f9b63fc2146d833ac1a6d4bc671be99bc7c
vikash sharma <119288896+vikashage21@users.noreply.github.com>
2026-05-31 13:57:24 +0000

    second pattern problem is solved

---
+++ pattern.js
@@ -0,0 +1,11 @@
+let n = 3;
+for (let i = 1; i <= 3; i++) {
+    // row 
+
+    let pattern = '';
+    for (let i = 1; i <= n; i++) {
+        // column
+        pattern += '*'
+    }
+    console.log(pattern)
+}

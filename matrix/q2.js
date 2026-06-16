4ef7ce32d6ba123841fbe520d68dc18fdd7077db
vikash sharma <vk220783@gmail.com>
2026-06-16 17:23:38 +0530

    column wise sum of matrix

---
+++ matrix/q2.js
@@ -0,0 +1,13 @@
+// column wise sum of matrixc
+let arr = [[3, 6, 9], [1, 7, 8], [2, 5, 4]]
+
+let row = arr.length;
+let col = arr.length;
+
+for (let i = 0; i < row; i++) {
+    let sum = 0;
+    for (let j = 0; j < col; j++) {
+        sum += arr[j][i]
+    }
+    console.log(sum)
+}
